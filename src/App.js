90a35131c70f19dfab5f831518eae06321a609f5
gfrnca <gabrielbfranca27@gmail.com>
2023-03-15 10:46:37 -0300

    Created Home Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+// General
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// CSS
 import GlobalStyle from "./styles/global.js";
+
+// Components
+import Home from "./pages/Home/Home.jsx";
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
         </Routes>
-
       </BrowserRouter>
     </div>
   );
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,11 @@
+import styled from "styled-components";
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle `
     // GLOBAL STYLES
     * {
-        
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
     }
 `
